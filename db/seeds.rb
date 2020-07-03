3d3a9588594dcabe105c5378596f89dd1bcb407c
alexpac27 <alexpac27@gmail.com>
2020-07-03 18:18:20 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Recipe.create(title: "Pasta Carbonara")
+Recipe.create(title: "French Bread")
+Recipe.create(title: "Raspberry Sorbet")
+
+Ingredient.create(name: "Sugar", quantity: "1 cup")
+Ingredient.create(name: "Flour", quantity: "1/2 cup")
+Ingredient.create(name: "Milk", quantity: "2 oz")
---
+++ test.rb
@@ -0,0 +1,12 @@
+params = {
+
+    recipe: {
+        title: "Date Salad"
+        ingredients_attributes: [
+            {name: "Ingredient 1 name", quantity: "quantity 1", recipe_id: "whatever the date salad is"}
+            {name: "Ingredient 2 name", quantity: "quantity 2", recipe_id: "whatever the date salad is_2"}
+        ]
+
+
+    }
+}
